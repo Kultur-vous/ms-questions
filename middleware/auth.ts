a69b44c8276bca1966b92fc3c497ad4dcb782eee
marth154 <37938187+marth154@users.noreply.github.com>
2021-12-01 20:39:41 +0100

    Heroku

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,12 +1,11 @@
-//import jwt from "jsonwebtoken";
-const jwt = require("jsonwebtoken");
+
+import { verify } from "jsonwebtoken";
 
 export default function auth(req: any, res: any, next: any) {
   const token = req.headers.authorization.split(" ")[1];
   try {
-    const decryptToken = jwt.verify(token, "shhhhh");
-    
-    if (decryptToken.email === req.headers.email) {
+    const decryptToken = verify(token, "shhhhh");
+    if (Object.values(decryptToken)[1] === req.headers.email) {
       next();
     } else {
       res.status(400).send({error:"Le token n'est pas valide"})
